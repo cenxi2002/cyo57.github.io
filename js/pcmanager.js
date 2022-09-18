ab70be79049081235aa3d8755524c64bf298f2c3
cyo57 <cyo57@outlook.com>
2022-09-18 18:43:04 +0800

    test

--- js/pcmanager.js
+++ js/pcmanager.js
@@ -1,49 +1,38 @@
-document.getElementById("startTimeButton").onclick = function(){
-    alert("成功");
-}
-
-function ajax(opt) {
-    opt = opt || {};
-    opt.method = opt.method.toUpperCase() || 'POST';
-    opt.url = opt.url || '';
-    opt.async = opt.async || true;
-    opt.data = opt.data || null;
-    opt.success = opt.success || function () {};
-    var xmlHttp = null;
-    if (XMLHttpRequest) {
-        xmlHttp = new XMLHttpRequest();
+$(document).ready(function () {
+    document.getElementById("startTimeButton").onclick = function () {
+        var a = $.get('https://open.iii.pics/iiimimi/detail')
+        alert(a);
     }
-    else {
-        xmlHttp = new ActiveXObject('Microsoft.XMLHTTP');
-    }var params = [];
-    for (var key in opt.data){
-        params.push(key + '=' + opt.data[key]);
-    }
-    var postData = params.join('&');
-    if (opt.method.toUpperCase() === 'POST') {
-        xmlHttp.open(opt.method, opt.url, opt.async);
-        xmlHttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded;charset=utf-8');
-        xmlHttp.send(postData);
-    }
-    else if (opt.method.toUpperCase() === 'GET') {
-        xmlHttp.open(opt.method, opt.url + '?' + postData, opt.async);
-        xmlHttp.send(null);
-    } 
-    xmlHttp.onreadystatechange = function () {
-        if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-            opt.success(JSON.parse( xmlHttp.responseText));//如果不是json数据可以去掉json转换
-        }
-    };
-}
+    /*
+    * @url: url link
+    * @action: "get", "post"
+    * @json: {'key1':'value2', 'key2':'value2'} 
+    */
+    
+});
 
 /*
-ajax({
-    method: 'POST',
-    url: 'url',
-    success: function (OriginalFromActivity) {
-        //在这里对获取的数据经常操作
-
-
+JavaScript Form GET/POST
+function doFormRequest(url, action, json) {
+    var form = document.createElement("form");
+    form.action = url;
+    form.method = action;
+    // append input attribute and valus
+    for (var key in json) {
+        if (json.hasOwnProperty(key)) {
+            var val = json[key];
+            input = document.createElement("input");
+            input.type = "hidden";
+            input.name = key;
+            input.value = val;
+            // append key-value to form
+            form.appendChild(input)
+        }
     }
-})
+    // send post request
+    document.body.appendChild(form);
+    form.submit();
+    // remove form from document
+    document.body.removeChild(form);
+}
 */
