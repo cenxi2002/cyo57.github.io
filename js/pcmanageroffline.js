59b489af6d42cec35a4ada5a5464e3c66cb2a248
cyo57 <cyo57@outlook.com>
2022-11-14 09:02:29 +0800

    offline bug fix 2

--- js/pcmanageroffline.js
+++ js/pcmanageroffline.js
@@ -4,5 +4,5 @@ function cal(s) {
     var pw90 = (s * 456789 + "").slice(-6)
     var pw120 = (s * 678987 + "").slice(-6)
     var pw999 = ((Number(s) + 3131 - 400) * 7422 + "").slice(-6)
-    alert((Number(s) + 3131 - 400) + "\n" + "30分钟: " + pw30 + "\n" + "60min: " + pw60 + "\n" + "90min: " + pw90 + "\n" + "120min: " + pw120 + "\n" + "999min: " +pw999)
+    alert("30分钟: " + pw30 + "\n" + "60min: " + pw60 + "\n" + "90min: " + pw90 + "\n" + "120min: " + pw120 + "\n" + "999min: " +pw999)
 }
