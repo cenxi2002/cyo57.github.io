dff63dbf193524cc6f3367f53cbd31a9c854ca1b
cyo57 <cyo57@outlook.com>
2022-11-14 08:59:52 +0800

    offline bug fix

--- js/pcmanagerVip.js
+++ js/pcmanagerVip.js
@@ -66,7 +66,7 @@ $(document).ready(function () {
         });
         get_info()
     });
-    $("btnOffile").click(function () {
+    $("#btnOffile").click(function () {
         window.location.replace("http://yunshangbandao.top/PcManagerOffline.html");
     })
 
