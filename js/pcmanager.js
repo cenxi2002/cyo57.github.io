42d4559cf64b2d9b9cadb16ee274add964189af6
cyo57 <cyo57@outlook.com>
2022-12-11 18:50:26 +0800

    bug fix

--- js/pcmanager.js
+++ js/pcmanager.js
@@ -3,41 +3,48 @@ $(document).ready(function () {
         var a = $.get('https://open.iii.pics/iiimimi/detail')
         alert(a);
     }*/
-    $.get("https://open.iii.pics/iiimimi/detail", function (data, status) {
-        obj = JSON.stringify(data) //转换为Json字符串
-        obj = JSON.parse(obj) //转换为Json对象
-        console.log(obj)
+    function get_info() {
+        postdata = { "key":"*-*" }
+        $.post("http://mihoyo.iii.pics:7114/mi_m_i/api/get", postdata, function (data, status) {
+            obj = JSON.stringify(data) //转换为Json字符串
+            obj = JSON.parse(obj) //转换为Json对象
+            console.log("get: " + obj)
 
-        var timeNow = parseInt(+new Date() / 1000) //当前10位时间戳
-        var _bootTime = "已持续开机 " + parseInt((timeNow - Number(obj.bootTime)) / 60) + " 分钟"
-        //console.log(timeNow, Number(obj.lastRequest))
-        if ((timeNow - Number(obj.lastRequest)) < 30) {
-            time = new Date(Number(obj.bootTime + "000")).toLocaleString() //bootTime
-            $("#textBootTime").text("开机时间: " + time)
-            $("#titleStatus").text("正在运行")
-            $("#titleLastExe").text("当前运行:" + obj.lastExe)
-            $("#textRunTime").text(_bootTime)
-        } else {
-            time = new Date(Number(obj.lastRequest + "000")).toLocaleString() //最后一次请求
-            $("#textBootTime").text("关机时间:" +time)
-            $("#textRunTime").text("已关机")
-            $("#titleStatus").text("已关机")
-            $("#titleLastExe").text("啊哦, 电脑没有运行哦")
-        }
-        if(obj.bootAllow == '0'){
-            $("#textLockStatus").text('已锁定')
-        }else if(obj.bootAllow == '1'){
-            $("#textLockStatus").text('未锁定')
-        }else{
-            $("#textLockStatus").text('无法获取')
-        }
-        if(obj.message == null){
-            $("#textMessage").text("留言:" + obj.message)
-        }else{
-            $("#textMessage").text("留言未设置")
-        }
-        //});
-    });
+            var timeNow = parseInt(+new Date() / 1000) //当前10位时间戳
+            var _bootTime = "已持续开机 " + parseInt((timeNow - Number(obj.data.bootTime)) / 60) + " 分钟"
+            //console.log(timeNow, Number(obj.lastRequest))
+            if ((timeNow - Number(obj.data.lastRequest)) < 30) {
+                time = new Date(Number(obj.data.bootTime + "000")).toLocaleString() //bootTime
+                $("#textBootTime").text("开机时间: " + time)
+                $("#titleStatus").text("正在运行")
+                $("#titleLastExe").text("当前运行:" + obj.data.lastExe)
+                $("#textRunTime").text(_bootTime)
+            } else {
+                time = new Date(Number(obj.data.lastRequest + "000")).toLocaleString() //最后一次请求
+                $("#textBootTime").text("关机时间:" + time)
+                $("#textRunTime").text("已关机")
+                $("#titleStatus").text("已关机")
+                $("#titleLastExe").text("啊哦, 电脑没有运行哦")
+            }
+            if (obj.data.bootAllow == '0') {
+                $("#textLockStatus").text('已锁定')
+            } else if (obj.data.bootAllow == '1') {
+                $("#textLockStatus").text('未锁定')
+            } else {
+                $("#textLockStatus").text('无法获取')
+            }
+
+            console.log("message: " + obj.data.message)
+            if (obj.data.message == "none") {
+                $("#textMessage").text("留言未设置")
+            } else {
+                $("#textMessage").text("留言:" + obj.data.message)
+            }
+            //});
+            return (obj)
+        });
+    }
+    get_info()
 
     $("#btnLockStatus").click(function(){
         console.log("btnLock clicked")
